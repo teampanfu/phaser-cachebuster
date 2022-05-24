53615cf7e7016cb5fd4848d99ea5b3c561b3e1bf
Robin <robin@panfu.us>
2022-05-24 18:00:33 +0200

    Update comment in type declaration

--- types/cachebuster.d.ts
+++ types/cachebuster.d.ts
@@ -1,7 +1,8 @@
 declare namespace Phaser.Loader {
     interface LoaderPlugin extends Phaser.Events.EventEmitter {
         /**
-         * The query parameter to append to the URL.
+         * The string that will be appended as query parameter to all loaded files.
+         * This can be for example a version number or a timestamp.
          */
         cacheBuster: string | number | null;
     }
